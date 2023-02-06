c3e5b741bacc956c60d699e5412ede6f317e4634
Lucas <lucasregner2002@gmail.com>
2023-02-06 16:54:09 -0300

    Dia uno

---
+++ src/components/Cards.jsx
@@ -0,0 +1,18 @@
+import Card from "./Card";
+
+export default function Cards(props) {
+  const { characters } = props;
+  return (
+    <div>
+      {characters.map((c) => (
+        <Card
+          name={c.name}
+          species={c.species}
+          gender={c.gender}
+          image={c.image}
+          onClose
+        />
+      ))}
+    </div>
+  );
+}
